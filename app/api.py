f40f3c24ad82a11ef489e5d4033612771f00c31d
XabiLR <152807524+XabiLR@users.noreply.github.com>
2023-12-30 08:46:14 +0100

    Update api.py
    cambios en divide para dividir por cero

--- app/api.py
+++ app/api.py
@@ -44,6 +44,8 @@ def multiply(op_1, op_2):
 def divide(op_1, op_2):
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        if num==0:
+            return("Cannot divide by zero",406,HEADERS)
         return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
